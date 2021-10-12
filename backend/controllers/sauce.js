514f30a33f6347cd98e317671fb9becf28078525
Jolanta KRZYZAK <jolanta.krzyzak@epitech.eu>
2021-10-12 14:58:41 +0200

    sauces

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,10 +1,11 @@
 const Sauce = require('../models/sauce');
 const fs = require('fs');
-const sauce = require('../models/sauce');
+
 
 /// CRÉER UNE SAUCE
     exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
+    console.log(sauceObject);
     delete sauceObject._id;
     const sauce = new Sauce({
         ...sauceObject,
@@ -16,7 +17,7 @@ const sauce = require('../models/sauce');
       });
       sauce.save()
         .then(() => res.status(201).json({message: 'Sauce enregistre'}))
-        .catch( error => res.status(400).json({error}));
+        .catch(error => res.status(400).json({error}));
   };
 
 /// MODIFIER UNE SAUCE
@@ -26,8 +27,8 @@ const sauce = require('../models/sauce');
         ...JSON.parse(req.body.sauce), 
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`} : { ...req.body}; 
     Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id}) 
-        .then(() => res.status(200).json({message: 'Sauce modifie'})) 
-        .catch(error => res.status(400).json({error})); 
+        .then(() => res.status(200).json({message: 'Sauce modifie'}))
+        .catch(error => res.status(400).json({error: 'error MODIFIER UNE SAUCE'})); 
 };
 
 
@@ -39,7 +40,7 @@ const sauce = require('../models/sauce');
         fs.unlink(`images/${filename}`, () => {
           Sauce.deleteOne({ _id: req.params.id })
             .then(() => res.status(200).json({ message: 'Sauce supprime'}))
-            .catch(error => res.status(400).json({error}));
+            .catch(error => res.status(400).json({error: 'error SUPPRIMER UNE SAUCE'}));
         });
       })
       .catch(error => res.status(500).json({error}));
@@ -49,7 +50,7 @@ const sauce = require('../models/sauce');
     exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({_id: req.params.id})
         .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({error}));
+        .catch(error => res.status(404).json({error: 'error AFFICHER UNE SEULE SAUCE'}));
   }; 
 
 
@@ -57,7 +58,7 @@ const sauce = require('../models/sauce');
     exports.getAllSauces = (req, res, next) => {
     Sauce.find()
       .then(sauces => res.status(200).json(sauces))
-      .catch(error => res.status(400).json({error}));
+      .catch(error => res.status(400).json({error: 'error AFFICHER TOUTES LES SAUCES'}));
     };
 
 
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -8,7 +8,7 @@ const multer = require('../middleware/multer-config');
 
 router.post('/', auth, multer, sauceCtrl.createSauce);  
 router.put('/:id', auth, multer, sauceCtrl.modifySauce); 
-router.delete('/:id', auth, multer, sauceCtrl.deleteSauce );
+router.delete('/:id', auth, sauceCtrl.deleteSauce );
 router.get('/', auth, multer, sauceCtrl.getAllSauces );
 router.get('/:id', auth, sauceCtrl.getOneSauce); 
 // router.post ('/:id/like', auth, sauceCtrl.likeSauce);
