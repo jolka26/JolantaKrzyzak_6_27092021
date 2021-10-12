5ffe2eba34b4e57ca3eaa68fb99367462753da9d
Jolanta KRZYZAK <jolanta.krzyzak@epitech.eu>
2021-10-12 18:37:24 +0200

    sauces vf

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    // res.setHeader('Content-Security-Policy', "default-src 'self'");
+    res.setHeader('Content-Security-Policy', "default-src 'self'");
     next();
   });
 
