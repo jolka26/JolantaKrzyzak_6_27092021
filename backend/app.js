1fa260ae1bf8b426983d1a9a0c5830c1df7c4412
Jolanta KRZYZAK <jolanta.krzyzak@epitech.eu>
2021-10-18 21:14:54 +0200

    modif/suppp/image

--- backend/app.js
+++ backend/app.js
@@ -23,8 +23,8 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Content-Security-Policy', "default-src 'self'");
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+    // res.setHeader('Content-Security-Policy', "default-src 'self'");
     next();
   });
 
--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -17,19 +17,37 @@ const fs = require('fs');
       });
       sauce.save()
         .then(() => res.status(201).json({message: 'Sauce enregistree'}))
-        .catch(error => res.status(400).json({error: 'error CRÉER UNE SAUCE'}));
+        .catch(error => res.status(400).json({error}));
   };
 
 /// MODIFIER UNE SAUCE
-    exports.modifySauce = (req, res, next) => { 
-    const sauceObject = req.file ? 
-    { 
-        ...JSON.parse(req.body.sauce), 
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`} : { ...req.body}; 
-    Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id}) 
-        .then(() => res.status(200).json({message: 'Sauce modifiee'}))
-        .catch(error => res.status(400).json({error: 'error MODIFIER UNE SAUCE'})); 
+  exports.modifySauce = (req, res, next) => {
+  if (req.file) {
+      // si l'image est modifiee, on supprime l'ancienne image dans le dossier /image
+      Sauce.findOne({ _id: req.params.id })
+          .then(sauce => {
+              const filename = sauce.imageUrl.split('/images/')[1];
+              fs.unlink(`images/${filename}`, () => {
+                  
+                  const sauceObject = {
+                      ...JSON.parse(req.body.sauce),
+                      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+                  }
+                  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+                      .then(() => res.status(200).json({ message: 'Sauce modifiee!' }))
+                      .catch(error => res.status(400).json({ error }));
+              })
+          })
+          .catch(error => res.status(500).json({ error }));
+  } else {
+      // si l'image n'est pas modifiee
+      const sauceObject = { ...req.body };
+      Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Sauce modifiee!' }))
+          .catch(error => res.status(400).json({ error }));
+  }
 };
+
 
 
 /// SUPPRIMER UNE SAUCE 
@@ -40,7 +58,7 @@ const fs = require('fs');
         fs.unlink(`images/${filename}`, () => {
           Sauce.deleteOne({ _id: req.params.id })
             .then(() => res.status(200).json({ message: 'Sauce supprimee'}))
-            .catch(error => res.status(400).json({error: 'error SUPPRIMER UNE SAUCE'}));
+            .catch(error => res.status(400).json({error}));
         });
       })
       .catch(error => res.status(500).json({error}));
@@ -50,7 +68,7 @@ const fs = require('fs');
     exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({_id: req.params.id})
         .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({error: 'error AFFICHER UNE SEULE SAUCE'}));
+        .catch(error => res.status(404).json({error}));
   }; 
 
 
@@ -58,7 +76,7 @@ const fs = require('fs');
     exports.getAllSauces = (req, res, next) => {
     Sauce.find()
       .then(sauces => res.status(200).json(sauces))
-      .catch(error => res.status(400).json({error: 'error AFFICHER TOUTES LES SAUCES'}));
+      .catch(error => res.status(400).json({error}));
     };
 
 
